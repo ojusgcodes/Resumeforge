47dc2303045311f64beac777d442633bd3f7f16d
ojusgcodes <ojusgupta2008@gmail.com>
2026-06-10 12:08:46 +0530

    Fix CORS for Vercel frontend

--- Backend/main.py
+++ Backend/main.py
@@ -23,13 +23,17 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://resumeforge-opal.vercel.app",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
